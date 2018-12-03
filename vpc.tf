4eae40eac38e928e27985b72e83a04a370e95efb
vikrant1992 <vikrant1013@gmail.com>
2018-12-03 19:48:30 +0530

    Update vpc.tf
    added private instace, NAT gateway and main route table

--- vpc.tf
+++ vpc.tf
@@ -81,6 +81,46 @@ egress {
   }
 }
 
+resource "aws_security_group" "sgapp" {
+  name = "vpc_test_app"
+  description = " app- SG-- Allow incoming HTTP connections & SSH access from public instance"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["10.0.1.0/24"]
+  }
+  
+egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.myvpc.id}"
+
+  tags {
+    Name = "Web Server SG"
+  }
+}
+
+
 resource "aws_internet_gateway" "IGW" {
   vpc_id      = "${aws_vpc.myvpc.id}"
 }
@@ -101,9 +141,34 @@ resource "aws_route_table_association" "public_RT" {
     route_table_id = "${aws_route_table.custom_RT.id}"
 }
 
+resource "aws_eip" "lb" {
+  instance = "${aws_instance.web.id}"
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "gw" {
+  allocation_id = "${aws_eip.lb.id}"
+  subnet_id     = "${aws_subnet.public.id}"
+}
 
 
+resource "aws_route_table" "main" {
+    vpc_id      = "${aws_vpc.myvpc.id}"
+    route{
+      cidr_block = "0.0.0.0/0"
+      nat_gateway_id = "${aws_nat_gateway.gw.id}"
+    }
+    tags {
+        Name = "main RT"
+    }
+}
+resource "aws_route_table_association" "public_RT" {
+    subnet_id = "${aws_subnet.subnet2.id}"
+    route_table_id = "${aws_route_table.main.id}"
+}
 
+
+#Instance public 
 resource "aws_instance" "web" {
   ami           = "ami-01bbe152bf19d0289"
   instance_type = "t2.micro"
@@ -121,3 +186,27 @@ resource "aws_instance" "web" {
   }
 
 }
+
+#Instance 2
+resource "aws_instance" "app" {
+  ami           = "ami-01bbe152bf19d0289"
+  instance_type = "t2.micro"
+  tenancy       = "default"
+  key_name      = "${aws_key_pair.my_key.key_name}"
+
+  subnet_id     = "${aws_subnet.subnet2.id}"
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.sgapp.id}"]
+  tags {
+      Name = "app"
+  }
+
+}
+
+
+
+
+
+
+
+
