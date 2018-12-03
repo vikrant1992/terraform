c2628a1f9f2ae4780005046a6e586f4d0879ca7a
root <root@ip-172-31-40-216.ec2.internal>
2018-12-03 11:03:32 +0000

    commited thre files. 1. public and private key. 2. terraform file

---
+++ vpc.tf
@@ -0,0 +1,123 @@
+provider "aws" {
+  access_key = "AKIAION65XYYF3YTUGIQ"
+  secret_key = "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"
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  tags {
+      Name = "myvpc"
+}
+
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id      = "${aws_vpc.myvpc.id}"
+  cidr_block   = "10.0.1.0/24"
+  tags {
+      Name = "public"
+  }
+
+
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id      = "${aws_vpc.myvpc.id}"
+  cidr_block   = "10.0.2.0/24"
+  tags {
+      Name = "private"
+  }
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name    = "vikk"
+  public_key  = "${file("vikk.pub")}"
+}
+
+resource "aws_security_group" "sgweb" {
+  name = "vpc_test_web"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+
+  vpc_id="${aws_vpc.myvpc.id}"
+
+  tags {
+    Name = "Web Server SG"
+  }
+}
+
+resource "aws_internet_gateway" "IGW" {
+  vpc_id      = "${aws_vpc.myvpc.id}"
+}
+
+
+resource "aws_route_table" "custom_RT" {
+    vpc_id      = "${aws_vpc.myvpc.id}"
+    route{
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.IGW.id}"
+    }
+    tags {
+        Name = "Custom_RT"
+    }
+}
+resource "aws_route_table_association" "public_RT" {
+    subnet_id = "${aws_subnet.subnet1.id}"
+    route_table_id = "${aws_route_table.custom_RT.id}"
+}
+
+
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-01bbe152bf19d0289"
+  instance_type = "t2.micro"
+  tenancy       = "default"
+  key_name      = "${aws_key_pair.my_key.key_name}"
+
+  subnet_id     = "${aws_subnet.subnet1.id}"
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+  
+  user_data	= "${file("bashscript.sh")}"
+
+  tags {
+      Name = "web"
+  }
+
+}
